6f4c970ebe6195fddd753388f5c9a76754254cfd
Eric <ericyuanovo@gmail.com>
2025-12-02 01:27:54 +0800

    feat: remove useless comment

--- src/core/content-script.tsx
+++ src/core/content-script.tsx
@@ -1,19 +1,10 @@
 /**
- * * content-script.ts(x) - Content script for Chrome Extension
- */
-
-/**
+ * * content-script.ts(x)
  * listen for messages from background
  */
 chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
-    if (message.type === "cleanup") {
-        /* // FIXME
-		window.removeEventListener("storage", window.__storage_event_handler!)
-		window.__storage_event_handler = null */
-        /* 	if (window.__localStorage_observer) {
-			clearInterval(window.__localStorage_observer)
-			window.__localStorage_observer = null
-		} */
-    }
-    return true;
-});
+	if (message.type === "cleanup") {
+		//
+	}
+	return true
+})
